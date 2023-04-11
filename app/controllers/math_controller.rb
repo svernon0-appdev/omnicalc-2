cc90904e6c2ab4b0f7f46d1b19474cbbd498097b
Spencer Vernon <svernon0@chicagobooth.edu>
2023-04-11 15:40:57 +0000

    Math edits

---
+++ app/controllers/math_controller.rb
@@ -0,0 +1,45 @@
+class MathController < ApplicationController
+
+  def wizard_add
+    @first = params.fetch("first_num").to_f
+
+    @second = params.fetch("second_num").to_f
+
+    @result = @first + @second
+
+    render({ :template => "math_templates/add_results.html.erb" })
+  end
+
+  def muggle_add
+    render({ :template => "math_templates/add_form.html.erb"})
+  end
+
+  def wizard_subtract
+    @first = params.fetch("first_num").to_f
+
+    @second = params.fetch("second_num").to_f
+
+    @result = @second - @first
+
+    render({ :template => "math_templates/subtract_results.html.erb" })
+  end
+
+  def muggle_subtract
+    render({ :template => "math_templates/subtract_form.html.erb"})
+  end
+
+  def wizard_multiply
+    @first = params.fetch("first_num").to_f
+
+    @second = params.fetch("second_num").to_f
+
+    @result = @first * @second
+
+    render({ :template => "math_templates/multiply_results.html.erb" })
+  end
+
+  def muggle_multiply
+    render({ :template => "math_templates/multiply_form.html.erb"})
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get("/wizard_add", { :controller => "math", :action => "wizard_add" })
+
+  get("/add", { :controller => "math", :action => "muggle_add" })
+
+  get("/wizard_subtract", { :controller => "math", :action => "wizard_subtract" })
+
+  get("/subtract", { :controller => "math", :action => "muggle_subtract" })
+
+  get("/wizard_multiply", { :controller => "math", :action => "wizard_multiply" })
+
+  get("/multiply", { :controller => "math", :action => "muggle_multiply" })
 end
